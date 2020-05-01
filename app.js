73c1d80e47a2333f57d9e38661055da7852d66d7
farid <farid.dinar.05@gmail.com>
2020-05-01 13:57:30 +0200

    firs JS app

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-var student={
-  name:"",
-  type:"student"
-};
-document.addEventListener("DOMContentLoaded", contentLoaded);
+(function (){
+'use strict';
 
-function contentLoaded(event){
-  document.getElementById('name').addEventListener("keyup", keyUp);
-}
+x='hello';
 
-function keyUp(event){
-  calculateNumericOutput();
-}
+angular.module('myFirstApp', [])
 
-function calculateNumericOutput(){
-  student.name = document.getElementById("name").value;
-  var total = 0;
-  for(var i=0; i<student.name.length; i++){
-    total += student.name.charCodeAt(i);
-  }
-var output = "total numeric of name is " + total;
-document.getElementsById('output').innerText = output;
-}
+.controller('myFirstController', function () {
+
+
+});
+
+})();
